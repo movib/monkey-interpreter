6a5b316915f0dae7fa9786c54194e809c08489b0
Bogdan Movileanu <bogdan.movileanu@uplead.com>
2024-10-04 17:20:58 +0300

    Fix todos, update repl

--- parser/parser.go
+++ parser/parser.go
@@ -134,7 +134,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -148,7 +148,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	stmt.ReturnValue = p.parseExpression(LOWEST)
 
-	for !p.curTokenIs(token.SEMICOLON) {
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
